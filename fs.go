7b4cfb56b7856fe86405c8ef984ab1bd5f20ce11
shivanisingh1804 <167208267+shivanisingh1804@users.noreply.github.com>
2024-05-18 17:31:01 +0530

    fs.go

--- fs.go
+++ fs.go
@@ -1,16 +1,3 @@
-// package main
-// import (
-//
-//	"net/http"
-//	"os"
-//
-// )
-// func main(
-//
-//	dir,_ := os.Getwd()
-//	http.ListenAndServe(":3000",FileServer(http.Dir(dir)))
-//
-// )
 package main
 
 import (
